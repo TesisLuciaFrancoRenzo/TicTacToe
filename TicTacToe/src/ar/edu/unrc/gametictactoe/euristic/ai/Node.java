6c95548528ee56735d0147a4d0613445bea31a4a
Franco Gastón Pellegrini <francogpellegrini@gmail.com>
2015-10-04 20:28:45 -0300

    Commit inicial para definir la inteligencia artificial euristica

---
+++ TicTacToe/src/ar/edu/unrc/gametictactoe/euristic/ai/Node.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ar.edu.unrc.gametictactoe.euristic.ai;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author franco
+ * @param <T>
+ */
+public class Node<T> {
+
+    private final ArrayList<Node<T>> childs;
+
+    private T value;
+
+    public Node() {
+        value = null;
+        childs = new ArrayList<>();
+    }
+
+    public Node(T value) {
+        this.value = value;
+        childs = new ArrayList<>();
+    }
+
+    /**
+     * @return the childs
+     */
+    public ArrayList<Node<T>> getChilds() {
+        return childs;
+    }
+
+    /**
+     * @return the value
+     */
+    public T getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public void addChild(Node<T> node) {
+        childs.add(node);
+    }
+
+    public boolean isLeave() {
+        return childs.isEmpty();
+    }
+}
