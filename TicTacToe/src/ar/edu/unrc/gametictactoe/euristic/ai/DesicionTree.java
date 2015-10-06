bc622eb974c93f5de80b0051ef0252f09bef604b
Franco Pellegrini <francogpellegrini@gmail.com>
2015-10-06 08:12:39 -0300

    Arbol de desiciones para la IA euristica implementado

--- TicTacToe/src/ar/edu/unrc/gametictactoe/euristic/ai/DesicionTree.java
+++ TicTacToe/src/ar/edu/unrc/gametictactoe/euristic/ai/DesicionTree.java
@@ -5,13 +5,47 @@
  */
 package ar.edu.unrc.gametictactoe.euristic.ai;
 
+import ar.edu.unrc.gametictactoe.Action;
+import ar.edu.unrc.gametictactoe.GameBoard;
+import ar.edu.unrc.gametictactoe.Player;
+import ar.edu.unrc.gametictactoe.Players;
+import ar.edu.unrc.gametictactoe.Token;
+import ar.edu.unrc.tdlearning.perceptron.interfaces.IAction;
+import java.util.ArrayList;
+
 /**
  *
  * @author franco
  */
 public class DesicionTree {
 
-    public DesicionTree() {
+    private Node<GameBoard> firstNode;
+    private long boardQuantity;
 
+    public DesicionTree(Player player1, Player player2) {
+        boardQuantity = 0;
+        GameBoard emptyBoard = new GameBoard(player1, player2);
+        firstNode = new Node<>();
+        firstNode.setValue(emptyBoard);
+        recursiveConstruction(firstNode);
+    }
+
+    private void recursiveConstruction(Node<GameBoard> node) {
+        ArrayList<IAction> possibleActions = node.getValue().listAllPossibleActions();
+        possibleActions.stream().forEach((a) -> {
+            GameBoard newBoard = (GameBoard) node.getValue().getCopy();
+            newBoard.pickSquare((Action) a);
+            Node<GameBoard> newNode = new Node<>();
+            newNode.setValue(newBoard);
+            firstNode.addChild(newNode);
+            boardQuantity++;
+            recursiveConstruction(newNode);
+        });
+    }
+
+    public static void main(String[] args) {
+        Player player1 = new Player("Player 1", 0, Token.X, Players.PLAYER1);
+        Player player2 = new Player("Player 2", 0, Token.O, Players.PLAYER2);
+        DesicionTree tree = new DesicionTree(player1, player2);
     }
 }
